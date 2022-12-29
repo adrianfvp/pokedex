0365da8c1ab2e0c6269395fac27a1b959fd40ab9
adrianfvp <adrianf2030@gmail.com>
2022-12-29 13:10:33 -0500

    diseño de tarjetas y unión con la api

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 import axios from "axios"
 
+const wrapper = document.querySelector('#wrapper')
+
 const pokemonDetail = async (url) => {
   const response = await axios.get(url)
   if (response.status === 200) {
     return response.data
+  } else {
+    return []
   }
 }
 
@@ -14,7 +18,33 @@ const getPokemons = async () => {
     const arrayPromises = test.map(async(item) => await pokemonDetail(item.url))
     Promise.allSettled(arrayPromises)
     .then(response => {
-      console.log(response)
+      const valores = response.map((item) => item.value)
+      const pokemons = valores.map(pokemon => {
+        return {
+          name: pokemon.name,
+          img: pokemon.sprites.other["official-artwork"].front_default,
+          stats: pokemon.stats
+        }
+      })
+      pokemons.forEach((pokemon) => {
+
+        const hp = pokemon.stats.find(stat => stat.stat.name === 'hp')
+        const attack = pokemon.stats.find(stat => stat.stat.name === 'attack')
+        const defense = pokemon.stats.find(stat => stat.stat.name === 'defense')
+        const card = document.createElement('div')
+        card.innerHTML = `
+        <div id="card" class="pokemon_card">
+        <h5>${pokemon.name}</h5>
+        <img src="${pokemon.img}" alt="imagen pokemon">
+        <div class="stats_elements">
+          <span>hp <strong>${hp.base_stat}</strong></span>
+          <span>attack <strong>${attack.base_stat}</strong></span>
+          <span>Defense <strong>${defense.base_stat}</strong></span>
+        </div>
+      </div>
+        `
+        wrapper.appendChild(card)
+      })
     })
   }
 }
